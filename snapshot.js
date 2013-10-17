4505e7752be5f95dd3d2a1503019bf95a9642305
Robin Berjon <robin@berjon.com>
2013-10-17 13:30:24 +0200

    create snapshotter

---
+++ snapshot.js
@@ -0,0 +1,12 @@
+
+
+// TODO
+//  - load up dom-core.html
+//  - generate the W3C headers using some local conf and ReSpec trickery
+//      - add a subtitle of the kind "Snapshot version of the <a href='...'>DOM Living Standard</a>"
+//      - try to make the list of editors reflect the genuine origin of the content
+//      - other changes?
+//  - save to index.html
+
+
+
